c7bcb83a9980fde79e921f44ca30b5a2c1ee4a4c
sangwoo24 <tjrtkddnaos@naver.com>
2022-09-06 10:00:05 +0900

    Xcode Project 생성

---
+++ wanted_pre_onboarding/wanted_pre_onboarding/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  wanted_pre_onboarding
+//
+//  Created by Jerry on 2022/09/06.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+}
+
